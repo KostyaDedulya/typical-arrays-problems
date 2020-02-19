273ac98d6f44eb29bf876e09c8d123d87d54434b
Konstantin Dedulya <eximal@gmail.com>
2020-02-19 22:13:11 +0300

    feat: solution for typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!arguments[0] || array.length === 0) return 0;
+  return array.sort((a,b) => b - a)[array.length - 1];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!arguments[0] || array.length === 0) return 0;
+  return array.sort((a,b) => b - a)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!arguments[0] || array.length === 0) return 0;
+  let sum = 0;
+  array.forEach(e => sum += e);
+  return sum / array.length;
 }
+
